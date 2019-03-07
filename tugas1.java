53b4b3ce34fbf579e284ea5ae80771c57f17d5a4
tiaraarista <48292571+tiaraarista@users.noreply.github.com>
2019-03-07 15:34:53 +0700

    Create tugas1.java

---
+++ tugas1.java
@@ -0,0 +1,8 @@
+public class BTiara {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Tiara Arista Hasanah");
+        System.out.println("NIM     : 17090010");
+        System.out.println("Kelas   : 4B");
+    }
+    
+}
